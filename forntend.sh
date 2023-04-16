fb9d1afba1ad2f87e20dfd808fd818ecefbdd119
samba <gudurusamba@gmail.com>
2023-04-16 09:25:17 +0530

    create forntend script

---
+++ forntend.sh
@@ -0,0 +1,11 @@
+yum install nginx
+
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+## some file needs to be created
+
+systemctl enable nginx
+systemctl start nginx
+
