0ae90411b0d5a8f7191536da54cc47c453405045
samba <gudurusamba@gmail.com>
2023-04-17 15:52:55 +0530

    create forntend script and configuration file
    create mongo .sh and mongo.repo
    forntend config file modified

--- forntend.sh
+++ forntend.sh
@@ -4,7 +4,7 @@ cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html || exit
 unzip /tmp/frontend.zip
 ## some file needs to be created
 
