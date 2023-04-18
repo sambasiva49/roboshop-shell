a7d21ce1b75d796299d20d790b7ae77edcc46544
samba <gudurusamba@gmail.com>
2023-04-18 16:20:13 +0530

    echo print staments

--- redis.sh
+++ redis.sh
@@ -10,4 +10,4 @@ sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
 echo -e "\e[36m>>>>>>>> start redis server <<<<<<<<<\e[0m"
 
 systemctl enable redis
-systemctl restart redis
+systemctl start redis
