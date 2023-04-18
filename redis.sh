98ac15cc489dfc6b1ae09cd3f8256284911285bd
samba <gudurusamba@gmail.com>
2023-04-18 16:23:53 +0530

    echo print staments

--- redis.sh
+++ redis.sh
@@ -1,13 +1,8 @@
-echo -e "\e[36m>>>>>>>> install redis repo <<<<<<<<<\e[0m"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 
-echo -e "\e[36m>>>>>>>> install redis  <<<<<<<<<\e[0m"
-#dnf module reset
 dnf module enable redis:remi-6.2 -y
 yum install redis -y
-echo -e "\e[36m>>>>>>>> update redis listen address <<<<<<<<<\e[0m"
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
-echo -e "\e[36m>>>>>>>> start redis server <<<<<<<<<\e[0m"
 
+sed -i -e 's|127.0.0.1|0.0.0.0' /etc/redis.conf /etc/redis/redis.conf
 systemctl enable redis
 systemctl start redis
