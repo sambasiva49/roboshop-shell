4c55d5d3793cf97523c27e8ae958c07001bf89cb
samba <gudurusamba@gmail.com>
2023-04-19 19:40:50 +0530

    add print statment

--- cart.sh
+++ cart.sh
@@ -1,22 +1,15 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
-
 rm -rf /app
 mkdir /app
 #cd /app
 curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
-
 unzip /tmp/cart.zip
-
-# shellcheck disable=SC2164
-cd /app
-
-npm install
 cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service
 
-
-systemctl daemon-reload
+npm install
+system daemon-reload
 systemctl enable cart
-systemctl satrt cart
+systemctl resatrt cart
