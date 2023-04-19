4c7de99680ed7a0cb7bfd1a21509cf0b99e0c9f2
samba <gudurusamba@gmail.com>
2023-04-19 10:35:18 +0530

    add print statment

--- catalogue.sh
+++ catalogue.sh
@@ -1,40 +1,40 @@
-echo -e "\e[36>>>>>>>>configuration nodejs repos<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>configuration nodejs repos<<<<<<<\e[0m"
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-echo -e "\e[36>>>>>>>>install nodejs  <<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>install nodejs  <<<<<<<\e[0m"
 
 yum install nodejs -y
-echo -e "\e[36>>>>>>>>add roboshop user <<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>add roboshop user <<<<<<<\e[0m"
 
 useradd roboshop
-echo -e "\e[36>>>>>>>> create application directory <<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>> create application directory <<<<<<<\e[0m"
 rm -rf /app
 mikdir /app
 cd /app
-echo -e "\e[36>>>>>>>> download app content <<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>> download app content <<<<<<<\e[0m"
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-echo -e "\e[36>>>>>>>>unzip app content <<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>unzip app content <<<<<<<\e[0m"
 
 unzip /tmp/catalogue.zip
-echo -e "\e[36>>>>>>>>copy catalogue systemd file <<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>copy catalogue systemd file <<<<<<<\e[0m"
 
 cp catalogue.service /etc/system.d/system/catalogue.service
-echo -e "\e[36>>>>>>>> install nodejs <<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>> install nodejs <<<<<<<\e[0m"
 
 npm install
-echo -e "\e[36>>>>>>>>start catalogue service<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>start catalogue service<<<<<<<\e[0m"
 
 system daemon-reload
 systemctl enable catalogue
 systemctl resatrt catalogue
 
-echo -e "\e[36>>>>>>>> copy mongo repo <<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>> copy mongo repo <<<<<<<\e[0m"
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-echo -e "\e[36>>>>>>>>install mongodb client <<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>install mongodb client <<<<<<<\e[0m"
 
 yum install mongodb-org-shell -y
-echo -e "\e[36>>>>>>>>load schema <<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>load schema <<<<<<<\e[0m"
 
 mongo --host mongodb.sambadevops.online </app/schema/catalogue.js
