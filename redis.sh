00371d21d7e4398566d8717401b06eedc8d808fc
samba <gudurusamba@gmail.com>
2023-04-19 19:17:46 +0530

    add print statment

--- redis.sh
+++ redis.sh
@@ -1,6 +1,7 @@
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 #dnf module reset
 dnf module enable redis:remi-6.2 -y
+
 echo -e "\e[36m>>>>>>>> redis insatall <<<<<<<\e[0m"
 
 yum install redis -y
