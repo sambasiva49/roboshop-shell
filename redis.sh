2be8c18b3a7abb71a7b0b37f7bd0a6dee18cadb7
samba <gudurusamba@gmail.com>
2023-04-19 16:01:37 +0530

    add print statment

--- redis.sh
+++ redis.sh
@@ -1,10 +1,10 @@
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-dnf module reset
+#dnf module reset
 dnf module enable redis:remi-6.2 -y
 echo -e "\e[36m>>>>>>>> redis insatall <<<<<<<\e[0m"
 
 yum install redis -y
 
-sed -i -e 's|127.0.0.1|0.0.0.0' /etc/redis.conf /etc/redis/redis.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
 systemctl enable redis
 systemctl start redis
