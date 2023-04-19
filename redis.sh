c1228de330d77be14d1583f36260cda50f1dc598
samba <gudurusamba@gmail.com>
2023-04-19 16:03:37 +0530

    add print statment

--- redis.sh
+++ redis.sh
@@ -5,6 +5,6 @@ echo -e "\e[36m>>>>>>>> redis insatall <<<<<<<\e[0m"
 
 yum install redis -y
 
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
 systemctl enable redis
 systemctl start redis
