1e71cdf4eaca53c8cc050012907b6154e6287bb7
samba <gudurusamba@gmail.com>
2023-04-19 16:33:44 +0530

    add print statment

--- cart.sh
+++ cart.sh
@@ -8,7 +8,7 @@ curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
 unzip /tmp/cart.zip
 
-cd /app
+#cd /app
 
 npm install
 cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service
