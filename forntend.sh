d3dfbf83bd6cb38e7ac8f6d77a6e855ca66e4cd3
samba <gudurusamba@gmail.com>
2023-04-19 11:26:42 +0530

    add print statment

--- forntend.sh
+++ forntend.sh
@@ -6,6 +6,7 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+
 systemctl restart nginx
 
 
