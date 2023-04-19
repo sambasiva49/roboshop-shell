59cc4d6e6bd77f7b3a1ffe824f5434a2c24e79a7
samba <gudurusamba@gmail.com>
2023-04-19 20:12:44 +0530

    add print statment

--- payment.sh
+++ payment.sh
@@ -1,8 +1,10 @@
 yum install python36 gcc python3-devel -y
 useradd roboshop
+rm -rf /app
 mkdir /app
 cd /app
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
+cd /app
 unzip /temp/payment.zip
 pip3.6 install -r requirements.txt
 cp /home/centos/roboshop-shell/payment.service /etc/systemd/system/payment.service
