3049d56bdbc5a757c5c1ee9abcf11adfc418e5b2
samba <gudurusamba@gmail.com>
2023-04-21 16:02:10 +0530

    add print statment

--- catalogue.sh
+++ catalogue.sh
@@ -1,4 +1,6 @@
-
+script=${realpath "$0"}
+script_path=$(dirname "$script")
+source "${script_path}"/common.sh
 echo -e "\e[36m>>>>>>>>configuration nodejs repos<<<<<<<\e[0m" | bash
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
@@ -8,7 +10,7 @@ yum install nodejs -y
 echo -e "\e[36m>>>>>>>>add roboshop user <<<<<<<\e[0m"
 
 
-useradd ${app_user}
+useradd "${app_user}"
 echo -e "\e[36m>>>>>>>> create application directory <<<<<<<\e[0m"
 rm -rf /app
 mkdir /app
@@ -16,6 +18,8 @@ cd /app
 echo -e "\e[36m>>>>>>>> download app content <<<<<<<\e[0m"
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
+
 echo -e "\e[36m>>>>>>>>unzip app content <<<<<<<\e[0m"
 
 unzip /tmp/catalogue.zip
@@ -25,7 +29,7 @@ cd /app
 npm install
 echo -e "\e[36m>>>>>>>>copy catalogue systemd file <<<<<<<\e[0m"
 
-cp ${script_path}/catalogue.service /etc/systemd/system/catalogue.service
+cp "${script_path}"/catalogue.service /etc/systemd/system/catalogue.service
 echo -e "\e[36m>>>>>>>>start catalogue service<<<<<<<\e[0m"
 
 system daemon-reload
