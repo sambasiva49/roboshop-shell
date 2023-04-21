a9a0ee4614cc4ca9e52ea55010d4a42557e45573
samba <gudurusamba@gmail.com>
2023-04-21 19:29:15 +0530

    add print statment

--- payment.sh
+++ payment.sh
@@ -1,15 +1,24 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+echo -e "\e[36m>>>>>>>>install python36<<<<<<<\e[0m"
 
 yum install python36 gcc python3-devel -y
+echo -e "\e[36m>>>>>>>>adding user<<<<<<<\e[0m"
+
 useradd ${app_user}
 rm -rf /app
+echo -e "\e[36m>>>>>>>>create diroctry<<<<<<<\e[0m"
+
 mkdir /app
 cd /app
+echo -e "\e[36m>>>>>>>>download file<<<<<<<\e[0m"
+
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
 cd /app
 unzip /tmp/payment.zip
+echo -e "\e[36m>>>>>>>>install python36<<<<<<<\e[0m"
+
 pip3.6 install -r requirements.txt
 echo -e "\e[36m>>>>>>>> copy the path<<<<<<<\e[0m"
 
