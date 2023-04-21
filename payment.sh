95287356a0de4f95c4ab45fbd949c8c4fd3b92ef
samba <gudurusamba@gmail.com>
2023-04-21 19:20:58 +0530

    add print statment

--- payment.sh
+++ payment.sh
@@ -11,7 +11,10 @@ curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.
 cd /app
 unzip /tmp/payment.zip
 pip3.6 install -r requirements.txt
+echo -e "\e[36m>>>>>>>> copy the path<<<<<<<\e[0m"
+
 cp ${script_path}/payment.service /etc/systemd/system/payment.service
+echo -e "\e[36m>>>>>>>> services restart<<<<<<<\e[0m"
 
 systemctl daemon-reload
 systemctl enable payment
