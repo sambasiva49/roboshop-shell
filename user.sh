250044f0668dba4cbd3867cdf2c90c75b95c5819
samba <gudurusamba@gmail.com>
2023-04-21 16:34:03 +0530

    add print statment

--- user.sh
+++ user.sh
@@ -1,8 +1,7 @@
-script_path=$(dirname $0)
+script=$(realpath "$0")
+script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-
-#exit
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd ${app_user}
